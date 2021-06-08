c83c6a4467d0dc5001fedaccde080b5f9702a0f3
Rodrigo <rodrigomendozaberna@gmail.com>
2021-06-08 17:55:30 -0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/test', function () {
+$router->post('/test', function () {
     return 'hello word';
 });
